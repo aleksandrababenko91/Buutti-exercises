e45a8f11c81a8deba4acf0801b1b4ffc6d5a82f4
Alex <aleksandrababenko91@gmail.com>
2023-10-16 08:29:08 +0300

    Extra

--- JavaScript/Extra/execise.js
+++ JavaScript/Extra/execise.js
@@ -19,14 +19,28 @@ console.log("Reversed numbers: ", reverseByMethods(numToString));
 
 //Write a JavaScript function that accepts a string as a 
 //parameter and converts the first letter of each word into upper case.
-const string = 'the fow brown';
-
+const string = "the quick brown fox";
+let newString = "";
 function convertToUpperCase(string) {
-    string.split(" ");
-    for(i = 0; i < string.length; i++){
-        string[i][0].toUpperCase() + string[i].slice(1);
-    }
-    return string.join;
+        newString = string.split(" ");
+        for(i = 0; i < newString.length; i++){
+        newString[i] = newString[i][0].toUpperCase() + newString[i].slice(1);
+   }
+        return newString.join(" ");
     }   
+console.log("Capitalized letter of each word: ", convertToUpperCase(string));
 
-console.log(convertToUpperCase(string));
+
+//3 Write a JavaScript function that accepts an argument and returns the type.
+//Note : There are six possible values that typeof returns: object, boolean, 
+//function, number, string, and undefined.
+
+const allValues = [{age: 32, surname: "Babenko"}, function upper(){}, true, 13, "Hello, world", undefined];
+let typesOfValues = "";
+function returnTheType(values) {
+    for(i = 0; i < allValues.length; i++) {
+    typesOfValues += typeof allValues[i] + " ";
+    }
+    return typesOfValues;
+}
+console.log("All DataTypes: ", returnTheType(allValues));
