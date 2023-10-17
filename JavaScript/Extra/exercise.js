81e06339c6da0a6653ff3b9b8fd651601a6cf290
Alex <aleksandrababenko91@gmail.com>
2023-10-17 10:42:19 +0300

    Extra

--- JavaScript/Extra/exercise.js
+++ JavaScript/Extra/exercise.js
@@ -3,7 +3,7 @@
 const numbers = 32243;
 let numToString = numbers.toString();
 let reversedNUmbers = "";
-function reverseNumbers(nums){
+function reverseNumbers(nums) {
     for(let i = numToString.length - 1; i >= 0; i--) {
         reversedNUmbers += numToString[i];
 }
@@ -11,7 +11,7 @@ function reverseNumbers(nums){
 }
 console.log("Reversed nember: ", reverseNumbers(numbers));
 
-function reverseByMethods(nums) { //I dont understand how to make parameters "num" light.
+function reverseByMethods(nums) { //I understand how to make parameters "num" light.
     return numToString.split("").reverse().join("");
 }
 console.log("Reversed numbers: ", reverseByMethods(numToString));
@@ -24,7 +24,7 @@ let newString = "";
 function convertToUpperCase(string) {
         newString = string.split(" ");
         for(i = 0; i < newString.length; i++){
-        newString[i] = newString[i][0].toUpperCase() + newString[i].slice(1);
+            newString[i] = newString[i][0].toUpperCase() + newString[i].slice(1);
    }
         return newString.join(" ");
     }   
@@ -39,7 +39,7 @@ const allValues = [{age: 32, surname: "Babenko"}, function upper(){}, true, 13, 
 let typesOfValues = "";
 function returnTheType(values) {
     for(i = 0; i < allValues.length; i++) {
-    typesOfValues += typeof allValues[i] + " ";
+        typesOfValues += typeof allValues[i] + " ";
     }
     return typesOfValues;
 }
