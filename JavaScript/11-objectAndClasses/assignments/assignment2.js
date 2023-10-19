aac7795a729542034de2944ea2816b0302a0454a
Alex <aleksandrababenko91@gmail.com>
2023-10-19 12:31:53 +0300

    Assignemnt2

--- JavaScript/11-objectAndClasses/assignments/assignment2.js
+++ JavaScript/11-objectAndClasses/assignments/assignment2.js
@@ -10,16 +10,21 @@ const vocabulary = {
     string: "merkkijono",
     network: "verkko"
 }
+//B
+const obj = { a: 2, b: 5, text: "hello, I am an example" };
+const printTranslatableWords = (myObject) => Object.keys(myObject); 
+console.log(printTranslatableWords(obj));
+console.log(printTranslatableWords(vocabulary));
 //C
-console.log("returns the corresponding word in Finnish.");
+console.log("returns the corresponding word in Finnish: ");
 const translate = word => vocabulary[word];
 
-console.log(translate("network")); // prints "verkko"
+console.log(translate("network")); 
 console.log(translate("hello"));
 console.log(translate("boolean"));
 console.log(translate("integer"));
-
 //D
-console.log("given word does not exist in the dictionary");
+console.log("Given word does not exist in the dictionary: ");
 const translate1 = word => vocabulary[word] ? vocabulary[word] : null;
 console.log(translate1("fhhfhfhf"));
+
