f50f6552251ec99e012bad7f85364949ccd350d1
Alex <aleksandrababenko91@gmail.com>
2023-10-23 15:35:57 +0300

    Classes and Objescts: Whole assignments

--- JavaScript/11-objectAndClasses/assignments/assignment3.js
+++ JavaScript/11-objectAndClasses/assignments/assignment3.js
@@ -1,15 +1,16 @@
 const getCountOfLetters = (string) => {
     let letterCount = {};
-    for(i = 0; i < string.length; i++) {
+    for(let i = 0; i < string.length; i++) {
         if(string[i] === " ") {
             continue;
-        } else if(letterCount.hasOwnProperty(string[i])) {
-            letterCount[string[i]]++;
-        } else {
+        } else if(!letterCount.hasOwnProperty(string[i])) {
             letterCount[string[i]] = 1;
+        } else {
+            letterCount[string[i]]++;
         }
     }
         return letterCount;
 }
 const result = getCountOfLetters("a black cat");
 console.log(result);
+    
