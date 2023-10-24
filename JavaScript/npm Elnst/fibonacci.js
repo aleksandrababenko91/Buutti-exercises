3aa3a35f75839fbbbe1ee5ac147744c3479ada69
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2023-10-24 13:40:45 +0300

    npm

---
+++ JavaScript/npm Elnst/fibonacci.js
@@ -0,0 +1,26 @@
+function fibonacci(n) {
+  let fibonacci = [];
+  const message = "Only positive numbers allowed";
+  let counter;
+
+  if (n < 0) {
+    return message;
+  } else if (n === 0) {
+    return fibonacci;
+  } else {
+    fibonacci = [1];
+    counter = 0;
+  }
+
+  let n1 = 0;
+  let n2 = 1;
+  while (counter < n) {
+    let sum = n1 + n2;
+    fibonacci.push(sum);
+    n1 = n2;
+    n2 = sum;
+    counter = counter + 1;
+  }
+  return fibonacci;
+}
+fibonacci();
