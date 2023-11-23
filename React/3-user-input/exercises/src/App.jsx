ca92023c4fca81ed45c33eab481d04de1469da84
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2023-11-23 11:24:33 +0200

    3-React ex 11 Delete Array Element

--- React/3-user-input/exercises/src/App.jsx
+++ React/3-user-input/exercises/src/App.jsx
@@ -5,6 +5,10 @@ import CounterButtonsArray from './CounterButtonsArray.jsx'
 import { useState } from 'react'
 import ArrayOfObjectsState from './ArrayOfObjectsState.jsx'
 import Counters from './Counters.jsx'
+import { v4 as uuidv4 } from 'uuid';
+import Counter from './Counter.jsx'
+
+
 
 function App() {
   const [value, setValue] = useState([0, 0, 0, 0]);
@@ -15,6 +19,17 @@ function App() {
     { id: 4, counter: 0 },
   ]);
 
+  const addNewElement = () => {
+    const newElement = {
+      id: uuidv4(),
+      counter: 0
+    }
+    setButtons([...buttons, newElement])  
+  } 
+
+  const deleteCounter = (id) => {
+    setButtons(buttons.filter(button => button.id !== id));
+};
 
   return (
     <div className="container" >
@@ -26,8 +41,20 @@ function App() {
         <CounterButtonsArray value={value} setValue={setValue}/>
         <h2>Exercise 7: </h2>
         <ArrayOfObjectsState buttons={buttons} setButtons={setButtons}/>
-        <h2>Exercise 8</h2>
+        <h2>Exercise 10</h2>
+        <button style={{marginTop: 20}} onClick={addNewElement}>Add a new counter!</button>
+        <h2>Exercise 8,9</h2>
         <Counters />
+        <div>
+          {buttons.map((button) => (
+        <Counter
+          key={button.id}
+          value={button.value}
+          onDeleteClick={() => deleteCounter(button.id)}
+        />
+      ))}
+    </div>
+        
     </div>
   );
 }
--- React/3-user-input/exercises/src/Counter.jsx
+++ React/3-user-input/exercises/src/Counter.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Counter = ({ id, counter,  onCounterClick }) => {
+const Counter = ({ id, counter,  onCounterClick, onDeleteClick }) => {
   return (
-    <button key={id} onClick={() =>  onCounterClick(id)}>
-      {counter}
-    </button>
+    <div>
+      <button key={id} onClick={() =>  onCounterClick(id)}>
+        {counter}
+      </button>
+      <button key={id} onClick={() => onDeleteClick(id)}>Delete</button>
+    </div>
   );
 };
 
@@ -13,5 +16,7 @@ Counter.propTypes = {
   id: PropTypes.number.isRequired,
   counter: PropTypes.number.isRequired,
   onCounterClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
 };
+
 export default Counter;
